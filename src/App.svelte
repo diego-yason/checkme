d5708c36e85eea59587d0d182632c35cb750df3c
KingMarine <61825575+KingMarine-GH@users.noreply.github.com>
2021-11-02 03:21:58 +0800

    moved the location of style tag

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,3 @@
-<style global lang="postcss">
-	@tailwind base;
-	@tailwind components;
-	@tailwind utilities;
-</style>
-
 <script lang="ts">
 	export let name: string;
 </script>
@@ -13,5 +7,8 @@
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
-<style>
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
 </style>
