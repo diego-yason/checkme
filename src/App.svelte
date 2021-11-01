eb053259bc8b0c8bac95b30f4300f34cb3fceaa8
KingMarine <61825575+KingMarine-GH@users.noreply.github.com>
2021-11-02 03:32:40 +0800

    changed templates

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-	export let name: string;
+    import Tailwind from "./Tailwind.svelte"
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<!-- Required -->
 
-<style global lang="postcss">
-	@tailwind base;
-	@tailwind components;
-	@tailwind utilities;
+<Tailwind />
+
+<!-- Required -->
+
+<style lang="scss">
+    
 </style>
